c28866e203c1d0b3f8b84dd3b8f8d06440612750
eyasaidi <eya.saidi@esprit.tn>
2023-11-16 01:09:17 +0100

    o

---
+++ src/test/java/tn/esprit/spring/kaddem/EquipeImplTest.java
@@ -0,0 +1,30 @@
+package tn.esprit.spring.kaddem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Equipe;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.services.IEquipeService;
+import tn.esprit.spring.kaddem.services.IEtudiantService;
+
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class EquipeImplTest {
+    @Autowired
+    IEquipeService equipeService;
+    @Test
+    @Order(1)
+    public void testRetrieveAllEquipes() {
+        List<Equipe> listEquipes = equipeService.retrieveAllEquipes();
+        Assertions.assertEquals(0, listEquipes.size());
+    }
+
+
+}
